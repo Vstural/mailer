500024d32a69eca1f3d82a79500e2ed8265ca91c
nzelites <nzelites@outlook.com>
2022-07-10 15:14:35 +0800

    misc:rename func

--- pkg/mailer/mailer.go
+++ pkg/mailer/mailer.go
@@ -8,7 +8,7 @@ import (
 	"github.com/Vstural/mailer/pkg/mailutils/outlook"
 )
 
-func SendMail(conf *config.Conf, to []string, title, content string) error {
+func SendOutlookMail(conf *config.Conf, to []string, title, content string) error {
 	auth := outlook.OutLookAuth(conf.UserName, conf.Password)
 
 	err := smtp.SendMail(fmt.Sprintf("%s:%d", conf.Server, conf.ServerPort),
